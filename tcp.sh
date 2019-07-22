9c80d22a19f7ce97523bea8b5273ab39f9f3aeb9
郑梦娟 <32807958+zhiyuanzmj@users.noreply.github.com>
2019-07-23 00:51:21 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -11,7 +11,8 @@ export PATH
 #=================================================
 
 sh_ver="1.3.1"
-github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
+#github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
+github="admin.allssr.com/release/Linux-NetSpeed"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
